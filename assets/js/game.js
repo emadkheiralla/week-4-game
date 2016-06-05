8bbd1b92b21aebc1a8cd01e44434fb932d66bb2a
Emad Kheiralla <Emad Kheiralla>
2016-06-05 04:39:39 -0400

    stillworking on jquery music file was corrupt

--- assets/js/game.js
+++ assets/js/game.js
@@ -2,11 +2,29 @@ $(document).ready(function () {
 
 	var isFirstChosen = false;
 	var isSecondChosen = false;
+
 	var firstChosen;
 	var secondChosen;
 
-	function fight(){
-
+	var luke = {
+		'healthMeter': 100,
+		'attackPower': 0,
+		'attackFactor': 5
+	}
+	var obie = {
+		'healthMeter': 120,
+		'attackPower': 0,
+		'attackFactor': 8
+	}
+	var yoda = {
+		'healthMeter': 150,
+		'attackPower': 0,
+		'attackFactor': 15
+	}
+	var darth = {
+		'healthMeter': 180,
+		'attackPower': 0,
+		'attackFactor': 25
 	}	   
 	
 	$('.char').on('click', function () {
@@ -88,9 +106,77 @@ $(document).ready(function () {
 	    	$('#yoda').animate({left: '-190px'});
 	       	secondChosen = 'luke';
 	       	isSecondChosen = true;
-	    }else if(isFirstChosen && isSecondChosen){
-	    	var snd = new Audio('../../assets/sounds/fight.mp3');
-			snd.play();
+	    }
+	});
+	
+	function fight(){
+		
+		if (this.firstChosen == 'luke' && this.secondChosen == 'obie') {
+			luke.attackPower += luke.attackFactor;
+			luke.healthMeter -= obie.attackFactor;
+			$('#attackedyoufor').html(obie.attackFactor);
+			$('#youattackedfor').html(luke.attackPower);
+		}else if (this.firstChosen == 'luke' && this.secondChosen == 'yoda') {
+			luke.attackPower += luke.attackFactor;
+			luke.healthMeter -= yoda.attackFactor;
+			$('#attackedyoufor').html(yoda.attackFactor);
+			$('#youattackedfor').html(luke.attackPower);
+		}else if (this.firstChosen == 'luke' && this.secondChosen == 'darth') {
+			luke.attackPower += luke.attackFactor;
+			luke.healthMeter -= darth.attackFactor;
+			$('#attackedyoufor').html(darth.attackFactor);
+			$('#youattackedfor').html(luke.attackPower);
+		}else if (this.firstChosen == 'obie' && this.secondChosen == 'luke') {
+			obie.attackPower += obie.attackFactor;
+			obie.healthMeter -= luke.attackFactor;
+			$('#attackedyoufor').html(luke.attackFactor);
+			$('#youattackedfor').html(obie.attackPower);
+		}else if (this.firstChosen == 'obie' && this.secondChosen == 'yoda') {
+			obie.attackPower += obie.attackFactor;
+			obie.healthMeter -= yoda.attackFactor;
+			$('#attackedyoufor').html(yoda.attackFactor);
+			$('#youattackedfor').html(obie.attackPower);
+		}else if (this.firstChosen == 'obie' && this.secondChosen == 'darth') {
+			obie.attackPower += obie.attackFactor;
+			obie.healthMeter -= darth.attackFactor;
+			$('#attackedyoufor').html(darth.attackFactor);
+			$('#youattackedfor').html(obie.attackPower);
+		}else if (this.firstChosen == 'yoda' && this.secondChosen == 'luke') {
+			yoda.attackPower += yoda.attackFactor;
+			yoda.healthMeter -= luke.attackFactor;
+			$('#attackedyoufor').html(luke.attackFactor);
+			$('#youattackedfor').html(yoda.attackPower);
+		}else if (this.firstChosen == 'yoda' && this.secondChosen == 'obie') {
+			yoda.attackPower += yoda.attackFactor;
+			yoda.healthMeter -= obie.attackFactor;
+			$('#attackedyoufor').html(obie.attackFactor);
+			$('#youattackedfor').html(yoda.attackPower);
+		}else if (this.firstChosen == 'yoda' && this.secondChosen == 'darth') {
+			yoda.attackPower += yoda.attackFactor;
+			yoda.healthMeter -= darth.attackFactor;
+			$('#attackedyoufor').html(darth.attackFactor);
+			$('#youattackedfor').text(yoda.attackPower);
+		}else if (this.firstChosen == 'darth' && this.secondChosen == 'luke') {
+			darth.attackPower += darth.attackFactor;
+			darth.healthMeter -= luke.attackFactor;
+			$('#attackedyoufor').html(luke.attackFactor);
+			$('#youattackedfor').html(darth.attackPower);
+		}else if (this.firstChosen == 'darth' && this.secondChosen == 'obie') {
+			darth.attackPower += darth.attackFactor;
+			darth.healthMeter -= obie.attackFactor;
+			$('#attackedyoufor').html(obie.attackFactor);
+			$('#youattackedfor').html(darth.attackPower);
+		}else if (this.firstChosen == 'darth' && this.secondChosen == 'yoda') {
+			darth.attackPower += darth.attackFactor;
+			darth.healthMeter -= yoda.attackFactor;
+			$('#attackedyoufor').html(yoda.attackFactor);
+			$('#youattackedfor').html(darth.attackPower);
+		}
+	}
+
+	$('#attack').on('click', function () {
+		
+		if(isFirstChosen && isSecondChosen){
 	    	fight();
 	    }
 	});
